5f21056fcfd68da92e09079ed4d3b45054cd7931
ggdream <ggdream@noreply.gitea.com>
2024-09-30 19:47:25 +0000

    更新 main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
+		w.Write([]byte("helfafalo"))
 	})
 	http.ListenAndServe(":9999", nil)
 }
